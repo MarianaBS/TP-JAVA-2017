69ac4ddf542b764c74ba00fbd129ae47e8314b1b
MarianaBS <marianabsanchez@hotmail.com>
2017-09-27 23:15:09 -0300

    prueba

---
+++ src/util/Fechas.java
@@ -0,0 +1,13 @@
+package util;
+import java.util.*;
+
+
+public class Fechas {
+	
+	public  int diferenciaEnDias2(Date fechaMayor, Date fechaMenor) {
+		 long diferenciaEn_ms = fechaMayor.getTime()- fechaMenor.getTime();
+		 long dias = diferenciaEn_ms / (1000 * 60 * 60 * 24);
+		 return (int) dias;
+		 }
+
+}
